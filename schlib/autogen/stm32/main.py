437140c62501fad05f6dcae3007245cd14c9f393
Hendrik v. Raven <hendrik@consetetur.de>
2017-07-31 18:42:05 +0200

    don't stumble upon added strings on pin numbers
    Some chips have pins called PA02_C. The old code tried to convert 02_C to
    an int and obviosly failed. Now we remove everything which is not a digit
    first.

--- schlib/autogen/stm32/main.py
+++ schlib/autogen/stm32/main.py
@@ -213,10 +213,10 @@ class device:
             if ((pin.pintype == "I/O" or pin.pintype == "Clock") and pin.name.startswith("P")):
                 port = pin.name[1]
                 try:
-                    self.ports[port][int(pin.name[2:])] = pin
+                    self.ports[port][int(re.sub("\D", "", pin.name[2:]))] = pin
                 except KeyError:
                     self.ports[port] = {}
-                    self.ports[port][int(pin.name[2:])] = pin
+                    self.ports[port][int(re.sub("\D", "", pin.name[2:]))] = pin
             elif (pin.pintype == "Clock"):
                 self.clockPins.append(pin)  
             elif ((pin.pintype == "Power") or (pin.name.startswith("VREF"))):
